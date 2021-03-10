78c6274dc0cbb2088457fc9f7509acf4a763fa36
tguo <tangguo1280@163.com>
2021-03-10 13:01:00 +0800

    delete models

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import numpy as np
 from dataload import *
+from model import *
 
 
 if __name__ == '__main__':
-    images, labels = load_mnist('data/MNIST/raw/')
-    print(images[0])
-    print(len(images))
-    print(len(labels))
+    # images, labels = load_mnist('data/MNIST/raw/')
+    # print(images[0])
+    # print(len(images))
+    # print(len(labels))
+    Linear_Net_models()
