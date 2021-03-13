dcf787bab4beb078ac9232f6a7a5924328036574
tguo <tangguo1280@163.com>
2021-03-13 16:16:39 +0800

    Update model.py

--- model.py
+++ model.py
@@ -180,6 +180,97 @@ def Linear_Net_models():
     print(f'Test Loss:{eval_loss / (len(test_dataset)):.6f},Acc:{eval_acc / (len(test_dataset)):.6f}')
 
 
+def Linear_models():
+    # 定义超参数
+    batch_size = 32
+    epoches = 2
+    learning_rate = 0.02
+
+    # 定义图片格式转化为Tensor格式：
+    '''
+    1.transforms.Compose()将各种预处理操作组合到一起
+    2.transform.ToTensor()将图片转换成 PyTorch 中处理的对象 Tensor.在转化的过程中 PyTorch 自动将图片标准化了，也就是说Tensor的范用是(0,1)之间
+    3.transforms.Normalize()要传入两个参数:均值、方差，做的处理就是减均值，再除以方差。将图片转化到了(-1,1)之间
+    4.注意因为图片是灰度图，所以只有一个通道，如果是彩色图片，有三个通道，transforms.Normalize([a,b,c],[d,e,f])来表示每个通道对应的均值和方差。
+    '''
+    data_tf = transforms.Compose([transforms.ToTensor(), transforms.Normalize([0.5], [0.5])])
+    # PyTorch 的内置函数 torchvision.datasets.MN工ST 导入数据集
+    train_dataset = datasets.MNIST(
+        root='./data', train=True, transform=data_tf, download=True)
+    test_dataset = datasets.MNIST(
+        root='./data', train=False, transform=data_tf)
+
+    # 注意：测试集为如下形式，评估的准确率为5.0，损失也很高。为什么：
+    # test_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
+    # torch.utils.data.DataLoader 建立一个数据迭代器，传入数据集和 batch size, 通过 shuffle=True 来表示每次迭代数据的时候是否将数据打乱。
+    # 测试集无需打乱顺序；训练集打乱顺序，为了增加训练模型的泛化能力
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    # 定义模型、损失函数和优化函数
+    # 输入的图片尺寸为28*28；两个隐层分别为300和100；最后输出维度是10，因为是10分类问题。
+    model = simpleNet(28 * 28, 300, 100, 10)
+    # 可以根据需要选择不同的模型进行分类任务
+    # model = Activation_Net(28 * 28, 300, 100, 10)
+    # model = Batch_Net(28 * 28, 300, 100, 10)
+
+    # 根据计算机当前硬件环境决定使用cpu还是gpu用于计算
+    if torch.cuda.is_available():
+        model = model.cuda()
+
+    criterion = nn.CrossEntropyLoss()  # 交叉熵损失
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate)  # 优化函数为随机梯度下降
+
+    # 训练模型
+    epoch = 0
+    for data in train_loader:
+        img, label = data
+        #     print(img.size())  #torch.Size([64, 1, 28, 28])
+        img = img.view(img.size(0), -1)  # 将维度变为(64,1*28*28)----(batch,in_dim)
+        if torch.cuda.is_available():
+            img = img.cuda()
+            label = label.cuda()
+        else:
+            img = Variable(img)
+            label = Variable(label)
+
+        # 此处为训练和迭代的主要部分
+        out = model(img)
+        loss = criterion(out, label)
+        print_loss = loss.data.item()
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        epoch += 1
+        if epoch % 50 == 0:
+            print(f'epoch: {epoch},Train Loss:{loss.data.item():.6f}')
+
+    # 模型评估
+    model.eval()
+    eval_loss = 0
+    eval_acc = 0
+    for data in test_loader:
+        img, label = data
+        img = img.view(img.size(0), -1)
+        if torch.cuda.is_available():
+            # with torch.no_grad()表示前向传播时不会保留缓存。测试集不需要做反向传播，所以可以在前向传播时释放掉内存，节约内存空间
+            with torch.no_grad():
+                img = Variable(img).cuda()
+                label = Variable(label).cuda()
+        else:
+            with torch.no_grad():
+                img = Variable(img)
+                label = Variable(label)
+        out = model(img)
+        loss = criterion(out, label)
+        eval_loss += loss.data.item() * label.size(0)
+        _, pred = torch.max(out, 1)
+        num_correct = (pred == label).sum()
+        eval_acc += num_correct.data.item()
+    print(f'Test Loss:{eval_loss / (len(test_dataset)):.6f},Acc:{eval_acc / (len(test_dataset)):.6f}')
+
+
 def CNN_model():
     torch.manual_seed(1)  # 为了每次的实验结果一致
     # 设置超参数
@@ -232,6 +323,7 @@ def CNN_model():
                 accuracy = ((pred_y == test_y.data.numpy()).astype(int).sum()) / float(test_y.size(0))
                 print('Epoch: ', epoch, '| train loss: %.4f' % loss.data.numpy(), '| test accuracy: %.4f' % accuracy)
 
+    torch.save(cnn, 'trained_models/cnn_01.pt')
     test_output = cnn(test_x[:10])
     pred_y = torch.max(test_output, 1)[1].data.numpy().squeeze()
     print(pred_y)
